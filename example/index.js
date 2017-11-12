65f25ddf2937039a85d0ebc1fae0607f789810e0
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-11-12 18:53:46 +0100

    chore(exaple): correctly render the page after the hook load

--- example/index.js
+++ example/index.js
@@ -16,11 +16,25 @@ const run = () => {
   );
 };
 
-window.addEventListener('message', ({ source, data: message }) => {
-  const isSameSource = source === window;
-  const isFromExtensionLoader = message && message.source === 'chrome-devtools-experiments-loader';
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line
+  const isHookAlreadyLoaded = !!window.__DEVTOOLS_EXPERIMENTS_HOOK__;
 
-  if (isSameSource && isFromExtensionLoader) {
+  if (!isHookAlreadyLoaded) {
+    // The hook could be laad before the page but it could be load after only
+    // in development mode so we need to plug a listener on the loader message
+    // in order to run the App after the hook load
+    window.addEventListener('message', ({ source, data: message }) => {
+      const isSameSource = source === window;
+      const isFromExtensionLoader = message && message.source === 'chrome-devtools-experiments-loader';
+
+      if (isSameSource && isFromExtensionLoader) {
+        run();
+      }
+    });
+  } else {
     run();
   }
-});
+} else {
+  run();
+}
